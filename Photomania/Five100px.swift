c6940e70e20835c192a53488c0bdae56581c6b2f
maselko <mateusz_fraczek@vp.pl>
2015-01-26 10:20:29 +0100

    1.5 Using Router

--- Photomania/Five100px.swift
+++ Photomania/Five100px.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct Five100px {
   enum ImageSize: Int {
@@ -15,6 +16,37 @@ struct Five100px {
     case Medium = 3
     case Large = 4
     case XLarge = 5
+    
+    enum Router: URLRequestConvertible {
+        static let baseURLString = "https://api.500px.com/v1"
+        static let consumerKey = "KJJAzOJIPGRA3sbwCNaLUDTYX6I8ryUQnpNU5U7X"
+        
+        case PopularPhotos(Int)
+        case PhotoInfo(Int, ImageSize)
+        case Comments(Int, Int)
+        
+        var URLRequest: NSURLRequest {
+            let (path: String, parameters: [String: AnyObject]) = {
+                switch self {
+                case .PopularPhotos (let page):
+                    let params = ["consumer_key": Router.consumerKey, "page": "\(page)", "feature": "popular", "rpp": "50",  "include_store": "store_download", "include_states": "votes"]
+                    return ("/photos", params)
+                case .PhotoInfo(let photoID, let imageSize):
+                    var params = ["consumer_key": Router.consumerKey, "image_size": "\(imageSize.rawValue)"]
+                    return ("/photos/\(photoID)", params)
+                case .Comments(let photoID, let commentsPage):
+                    var params = ["consumer_key": Router.consumerKey, "comments": "1", "comments_page": "\(commentsPage)"]
+                    return ("/photos/\(photoID)/comments", params)
+                }
+                }()
+            
+            let URL = NSURL(string: Router.baseURLString)
+            let URLRequest = NSURLRequest(URL: URL!.URLByAppendingPathComponent(path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            
+            return encoding.encode(URLRequest, parameters: parameters).0
+        }
+    }
   }
   
   enum Category: Int, Printable {
